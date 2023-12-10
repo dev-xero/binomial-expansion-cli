153c9324340d2962e08dd2f9f6ff3765de844581
Xero kun <elo48858@gmail.com>
2023-12-10 20:08:46 +0100

    feat: verify input before extraction

--- binomial/binomial.py
+++ binomial/binomial.py
@@ -1,8 +1,17 @@
 # Binomial Expansion Class
 class BinomialExpansion:
     def __init__(self, args):
-        args = args.get("exp").split()            # remove whitespace
-        self.args = self.extract_variables(args)  # extract variables
+        self.verify_arguments(args.get("exp"))          # verify arguments
+        args_array = args.get("exp").split()            # split arguments
+        self.args = self.extract_variables(args_array)  # extract variables
+
+    @staticmethod
+    def verify_arguments(args):
+        required_chars = {'(', ')', '^'}
+        if len(args) < 3 or not all(char in args for char in required_chars):
+            if '+' not in args or '-' not in args:
+                print("[ERR]: invalid expression encountered")
+                exit(1)  # terminate
 
     @staticmethod
     def extract_variables(args):
